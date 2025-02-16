81c06a352b480e4c6155abce1aeff18793d4a425
E.J.Russell <MakeSingularity@gmail.com>
2025-02-16 15:40:44 -0800

    fixed the requitements.txt file to remove the pygobject library as it waas not compatible with windows and pi.

--- update_pi.sh
+++ update_pi.sh
@@ -11,6 +11,8 @@ rsync -avz --exclude 'venv' --exclude '.git' --exclude '__pycache__' . $PI_USER@
 # SSH into Raspberry Pi and install dependencies
 ssh $PI_USER@$PI_HOST << 'ENDSSH'
 cd /home/pi/robot-droid-project
+sudo apt-get update
+sudo apt-get install -y python3-gi
 pip install -r requirements.txt
 ENDSSH
 
